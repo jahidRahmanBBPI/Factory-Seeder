8128dc17102242efab7f88f5227942a2dc0485c7
Jahid Rahman <rahmanjahid645@gmail.com>
2026-04-27 21:55:44 +0600

    added commends

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,15 +17,33 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
-        Person::factory()->count(5)->create();
+        Person::factory()->count(5000)->create();
 
-        // User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        // When you want to create a specific user with specific data, you can use the following code:
+
+        User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+        ]);
+
+        User::factory()->create([
+            'name' => 'Admin User',
+            'email' => 'test@example.com',
+        ]);
+
+        User::factory()->create([
+            'name' => 'editor',
+            'email' => 'test@example.com',
+        ]);
 
         $this->call([
             PersonSeeder::class,
         ]);
+        // php artisan db:seed [This is the command to run the seeder in terminal]
+        // php artisan migrate:refresh --seed [This is the command to refresh the database and run the seeder in terminal]
+        // php artisan db:seed --class=PersonSeeder [This is the command to run a specific seeder in terminal]
+        // php artisan make:model person -mfsc [This is the command to create model, migration, factory and seeder for person in terminal]
+
+        // php artisan make:migration rename_column_to_people_table --table=people [This is the command to create a migration to rename a column in people table in terminal]
     }
 }
