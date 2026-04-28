3a5d997eda42d58534dab6a9ef2adcfcb8719a54
Jahid Rahman <rahmanjahid645@gmail.com>
2026-04-28 23:35:34 +0600

    Working on image upload process

---
+++ app/Http/Controllers/UploadImageController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UploadImageController extends Controller
+{
+    function upload_image()
+    {
+        return view('UploadImage');
+    }
+
+    function img_upload(Request $request)
+    {
+        // return dd($request->all());
+
+        // Full Name: image
+        // dd($request->image->getClientOriginalName());
+
+        //Extension
+        // dd($request->image->extension());
+
+        // Size
+        // dd($request->image->getSize());
+
+
+        
+        // $request->validate([
+        //     'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+        // ]);
+
+        // $imageName = time() . '.' . $request->image->extension();
+        // $request->image->move(public_path('images'), $imageName);
+
+        // return back()->with('success', 'Image uploaded successfully.')->with('image', $imageName);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UploadImageController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', [HomeController::class, 'home'])->name('home');
+Route::get('/upload/image', [UploadImageController::class, 'upload_image'])->name('upload.image');
+Route::get('/upload+image', [UploadImageController::class, 'img_upload'])->name('img.upload');
