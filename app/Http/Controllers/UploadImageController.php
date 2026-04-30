ed6ab17dad49ef866f6ca38efacd53d2afc4b75e
Jahid Rahman <rahmanjahid645@gmail.com>
2026-05-01 01:35:54 +0600

    single image upload process

--- app/Http/Controllers/UploadImageController.php
+++ app/Http/Controllers/UploadImageController.php
@@ -33,8 +33,18 @@ class UploadImageController extends Controller
         // $imageName = 'photo' . md5(uniqid()) . time() . '.' . $request->image->extension();
 
         // dd($imageName);
-        // $request->image->move(public_path('images'), $imageName);
+        // $request->image->move(public_path('assets/uploads'), $imageName);
 
         // return back()->with('success', 'Image uploaded successfully.')->with('image', $imageName);
+
+        // image upload process (ostad)
+        $request->validate([
+            'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+        ]);
+        // image upload 
+        $imageName = 'photo' . md5(uniqid()) . time() . '.' . $request->image->extension();
+        $request->image->move(public_path('assets/uploads'), $imageName);
+
+        return redirect()->back();
     }
 }
